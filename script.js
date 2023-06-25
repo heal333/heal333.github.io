0f35b5752d515a8de75f750ad2c6ad4ed71b8b13
heal333 <rohanaich333@gmail.com>
2023-06-25 23:08:34 +0530

    scrolling effects implementation

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+"use strict";
+
 const scroll = document.querySelector(".scroll");
 const section3 = document.querySelector(".section3");
+const sidebar = document.querySelector(".sidebar");
 // let viewHeight = window.innerHeight * 0.4;
 let scrollPosition = 0;
+const headerDetails = document.querySelector(".headerDetails");
+const headerProj = document.querySelector(".headerProj");
+const headerEdu = document.querySelector(".headerEdu");
+const headerContact = document.querySelector(".headerContact");
+const hire = document.querySelector(".hire");
+const backdrop = document.querySelector(".backdrop");
+const contact = document.querySelector(".contact");
+const contactClose = document.querySelector(".contactClose");
+const header = document.querySelector("header");
 
 // window.addEventListener("resize", function () {
 //   viewHeight = window.innerHeight * 0.4;
@@ -9,11 +21,81 @@ let scrollPosition = 0;
 // });
 
 //to render the sidebar according to the scroll position
+
+const headerList = [headerDetails, headerProj, headerEdu, headerContact];
 function scrollEffect(location) {
-  scrollPosition = (window.scrollY / 3500) * 400;
-  scrollPosition < 380
+  headerList.forEach((elem) => {
+    elem.classList.remove("headerFocus");
+  });
+  scrollPosition = location / 11.5; //(window.scrollY / 3500) * 400;
+  scroll.style.translate = `-2px ${scrollPosition}px`;
+  location <= 4320
     ? (scroll.style.translate = `-2px ${scrollPosition}px`)
-    : (scroll.style.translate = `-2px ${380}px`);
+    : (scroll.style.translate = `-2px ${375.65}px`);
+  if (location < 880) {
+    headerDetails.classList.add("headerFocus");
+  } else if (location < 1960 && location >= 880) {
+    headerProj.classList.add("headerFocus");
+  } else if (location < 3040 && location >= 1960) {
+    headerEdu.classList.add("headerFocus");
+  } else if (location < 4120 && location >= 3040) {
+    headerContact.classList.add("headerFocus");
+  } else if (location >= 4120) console.log("end");
 }
 
-window.addEventListener("scroll", scrollEffect);
+// function sidebarSize(location) {
+//   if (location < 880) {
+//     headerDetails.classList.add("headerFocus");
+//   } else if (location < 1960 && location >= 880) {
+//     headerProj.classList.add("headerFocus");
+//   } else if (location < 3040 && location >= 1960) {headerEdu.classList.add("headerFocus")}
+//   else if (location < 4120 && location >= 3040) {headerContact.classList.add("headerFocus")}
+//   else if (location >= 4120) console.log("end");
+// }
+
+window.addEventListener("scroll", () => {
+  scrollEffect(window.scrollY);
+});
+
+//using event bubbling
+sidebar.addEventListener("click", function (event) {
+  const clickedOn = event.target.className.split(" ")[0]; //to get only the first class
+  if (clickedOn === "side0") window.scrollTo({ top: 0, behavior: "smooth" });
+  else if (clickedOn === "side1")
+    window.scrollTo({ top: 1080, behavior: "smooth" });
+  else if (clickedOn === "side2")
+    window.scrollTo({ top: 2160, behavior: "smooth" });
+  else if (clickedOn === "side3")
+    window.scrollTo({ top: 3240, behavior: "smooth" });
+  else if (clickedOn === "side4")
+    window.scrollTo({ top: 4320, behavior: "smooth" });
+});
+header.addEventListener("click", function (event) {
+  const clickedOn = event.target.className.split(" ")[0]; // to prevent bugs when adding multiple class
+  console.log(clickedOn);
+  if (clickedOn === "headerDetails")
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  else if (clickedOn === "headerProj")
+    window.scrollTo({ top: 1080, behavior: "smooth" });
+  else if (clickedOn === "headerEdu")
+    window.scrollTo({ top: 2160, behavior: "smooth" });
+  else if (clickedOn === "headerContact")
+    window.scrollTo({ top: 3240, behavior: "smooth" });
+  else if (clickedOn === "hire") {
+    backdrop.classList.remove("hidden");
+    contact.classList.remove("hidden");
+  }
+});
+
+hire.addEventListener("click", function () {
+  backdrop.classList.remove("hidden");
+  contact.classList.remove("hidden");
+});
+contactClose.addEventListener("click", function () {
+  backdrop.classList.add("hidden");
+  contact.classList.add("hidden");
+});
+backdrop.addEventListener("click", function () {
+  backdrop.classList.add("hidden");
+  contact.classList.add("hidden");
+});
